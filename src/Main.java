ab342c67a4c15de3d863f45c4cd9c4f07c81cc71
Maks Nikolenko <maks1803@yandex.ru>
2023-05-25 22:58:37 +0300

    second commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+            String text = "aba aba a!a abba adca abea";
+            Pattern pattern = Pattern.compile("a..a");
+
+            Matcher matcher = pattern.matcher(text);
+            while (matcher.find());
+                    {
+            System.out.println(matcher.replaceAll(""));
+                                           }
+        }
+    }
+
